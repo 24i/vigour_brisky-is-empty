24a9a10a0cff9d4cea353598d3a3f3f5ab0d6cd0
Shawn Inder <shawn@vigour.io>
2016-08-16 21:49:28 -0400

    Ported from vigour-util

---
+++ README.md
@@ -0,0 +1,12 @@
+# brisky-is-empty
+
+<!-- VDOC.badges travis; standard; npm; coveralls -->
+
+<!-- VDOC.jsdoc isEmpty -->
+
+```javascript
+var isEmpty = require('brisky-is-empty')
+var Base = require('vigour-base')
+isEmpty(new Base({})) // true
+isEmpty(new Base({ awesome: true })) // false
+```
---
+++ test/index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const test = require('tape')
+const Base = require('vigour-base')
+const isEmpty = require('../')
+const testCases = [
+// ['object', expectedResult]
+  [{}, true],
+  [{ a: true }, false],
+  [new Base({}), true],
+  [new Base({ a: 'a' }), false],
+  [new Base({ a: { val: null } }), true],
+  [new Base({ a: { val: void 0 } }), false],
+  [new Base({ properties: { shawn: true }, shawn: {} }), true]
+]
+
+test('isEmpty - base', function (t) {
+  t.plan(testCases.length)
+  testCases.forEach(function (item) {
+    t.equals(isEmpty(item[0]), item[1], 'isEmpty(' + item[0] + ') === ' + item[1])
+  })
+})
