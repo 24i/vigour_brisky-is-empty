ef1b4395decafefbcdf19df41d28cfc3127fcc08
Jim de Beer <jim@vigour.io>
2016-10-19 15:47:51 +0200

    added yarn, dependend on brisky-base

--- test/index.js
+++ test/index.js
@@ -1,20 +1,19 @@
 'use strict'
-
 const test = require('tape')
-const Base = require('vigour-base')
+const base = require('brisky-base')
 const isEmpty = require('../')
 const testCases = [
 // ['object', expectedResult]
   [{}, true],
   [{ a: true }, false],
-  [new Base({}), true],
-  [new Base({ a: 'a' }), false],
-  [new Base({ a: { val: null } }), true],
-  [new Base({ a: { val: void 0 } }), false],
-  [new Base({ properties: { shawn: true }, shawn: {} }), true]
+  [base({}), true],
+  [base({ a: 'a' }), false],
+  [base({ a: { val: null } }), true],
+  [base({ a: { val: void 0 } }), false],
+  [base({ properties: { shawn: true }, shawn: {} }), true]
 ]
 
-test('isEmpty - base', function (t) {
+test('isEmpty - base', t => {
   t.plan(testCases.length)
   testCases.forEach(function (item) {
     t.equals(isEmpty(item[0]), item[1], 'isEmpty(' + item[0] + ') === ' + item[1])
